4aa7d8bd9cab58ea580ed8ac6b142fb7bce9c985
Adrián González <adrigonzalezcabado@gmail.com>
2025-05-21 21:37:33 +0200

    Scripts predeterminados antes de que los toque

--- Assets/Scripts/WorldPlayerNoIA.cs
+++ Assets/Scripts/WorldPlayerNoIA.cs
@@ -1,16 +1,41 @@
+using Unity.Netcode;
 using UnityEngine;
 
-public class WorldPlayerNoIA : MonoBehaviour
+namespace HelloWorld
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class WorldPlayerNoIA : NetworkBehaviour
     {
-        
-    }
+        public NetworkVariable<Vector3> Position = new NetworkVariable<Vector3>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public override void OnNetworkSpawn()
+        {
+            if (IsOwner)
+            {
+                Move();
+            }
+        }
+
+        public void Move()
+        {
+            SubmitPositionRequestRpc();
+        }
+
+        [Rpc(SendTo.Server)]
+        private void SubmitPositionRequestRpc(RpcParams rpcParams = default)
+        {
+            var randomPosition = GetRandomPositionOnPlane();
+            transform.position = randomPosition;
+            Position.Value = randomPosition;
+        }
+
+        static Vector3 GetRandomPositionOnPlane()
+        {
+            return new Vector3(Random.Range(-3f, 3f), 1f, Random.Range(-3f, 3f));
+        }
+
+        private void Update()
+        {
+            transform.position = Position.Value;
+        }
     }
 }
